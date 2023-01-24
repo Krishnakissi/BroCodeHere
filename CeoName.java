15b8f29c4a7042da5da58878990946c8d68ecf6c
Krishna Kumar N <113898309+Krishnakissi@users.noreply.github.com>
2023-01-24 20:42:42 +0530

    Add files via upload

---
+++ CeoName.java
@@ -0,0 +1,117 @@
+class CompanyName{
+
+static String CeoName (String name)
+	{
+		
+	if (name=="Google")
+	{	
+	return "Sundar Pichai";
+	}
+
+	if(name=="Apple")
+	{	
+	return "Tim_Cook";
+	}
+
+	if(name=="Facebook")
+	{
+		return "Mark Zukerberg";
+	}
+
+	if(name=="HP")
+	{
+		return "Enrique Lores";
+	}
+
+	if(name=="Royal Enfield")
+	{
+		return "B Govindarajan";
+	}
+
+
+	if(name=="TVS")
+	{
+		return "K N Radhakrishnan";
+	}
+
+
+	if(name=="Lomborgini")
+	{
+		return "Stephan Winkelmann";
+	}
+
+
+	if(name=="Microsoft")
+	{
+		return "K N Radhakrishnan";
+	}
+
+
+	if(name=="Ambuja Cements")
+	{
+		return "Neeraj Akhoury";
+	}
+
+
+	if(name=="Amul")
+	{
+		return "R.S.Sodhi";
+	}
+
+	if(name=="Apollo Hospitals")
+	{
+		return "Prathap C Reddy";
+	}
+
+	if(name=="Bharat Petroleum")
+	{
+		return "Mr S Varadarajan";
+	}
+
+
+	if(name=="Britannia")
+	{
+		return "Varun Berry";
+	}
+
+	if(name=="Cafe Coffee Day")
+	{
+		return "Kalyan Krishnamurthy";
+	}
+
+	if(name=="Flipkart")
+		{
+			return "Malavika Hegde";
+		}
+
+	if(name=="HCL Technologies")
+			{
+				return "C Vijayakumar";
+			}
+
+
+	if(name=="Spice Jet")
+			{
+				return "Ajay Singh";
+			}
+
+
+	if(name=="Toyota")
+			{
+				return "Akio Toyoda";
+			}
+
+	if(name=="Jindal Steel")
+			{
+				return "Naveen Jindal";
+			}
+
+	if(name=="Reliance Industries")
+			{
+				return "Mukesh Ambani";
+			}
+
+	return "invalid";
+	}
+	}
+	
---
+++ CeoNameRunner.java
@@ -0,0 +1,66 @@
+class CeoNameRunner
+{
+public static void main(String[]CeoName)
+{
+String ref= CompanyName.CeoName("Google");
+System.out.println(ref+"--Ceo of Google");
+
+String ref1= CompanyName.CeoName("Apple");
+System.out.println(ref1+"--Ceo of Apple");
+
+String ref2= CompanyName.CeoName("Facebook");
+System.out.println(ref2+"--Ceo of Facebook");
+
+String ref3= CompanyName.CeoName("HP");
+System.out.println(ref3+"--Ceo of HP");
+
+String ref4= CompanyName.CeoName("Royal Enfield");
+System.out.println(ref4+"--Ceo of Royal Enfield");
+
+String ref5= CompanyName.CeoName("TVS");
+System.out.println(ref5+"--Ceo of TVS");
+
+String ref6= CompanyName.CeoName("Lomborgini");
+System.out.println(ref6+"--Ceo of Lomborgini");
+
+String ref7= CompanyName.CeoName("Microsoft");
+System.out.println(ref7+"--Ceo of Microsoft");
+
+String ref8= CompanyName.CeoName("Ambuja Cements");
+System.out.println(ref8+"--Ceo of Ambuja Cements");
+
+String ref9= CompanyName.CeoName("Amul");
+System.out.println(ref9+"--Ceo of Amul");
+
+String ref10= CompanyName.CeoName("Apollo Hospitals");
+System.out.println(ref10+"--Ceo of Apollo Hospitals");
+
+String ref11= CompanyName.CeoName("Bharat Petroleum");
+System.out.println(ref11+"--Ceo of Bharat Petroleum");
+
+String ref12= CompanyName.CeoName("Britannia");
+System.out.println(ref12+"--Ceo of Britannia");
+
+String ref13= CompanyName.CeoName("Cafe Coffee Day");
+System.out.println(ref13+"--Ceo of Cafe Coffee Day");
+
+String ref14= CompanyName.CeoName("Flipkart");
+System.out.println(ref14+"--Ceo of Flipkart");
+
+String ref15= CompanyName.CeoName("HCL Technologies");
+System.out.println(ref15+"--Ceo of HCL Technologies");
+
+String ref16= CompanyName.CeoName("Spice Jet");
+System.out.println(ref16+"--Ceo of Spice Jet");
+
+String ref17= CompanyName.CeoName("Toyota");
+System.out.println(ref17+"--Ceo of Toyota");
+
+String ref18= CompanyName.CeoName("Jindal Steel");
+System.out.println(ref18+"--Ceo of Jindal Steel");
+
+String ref19= CompanyName.CeoName("Reliance Industries");
+System.out.println(ref19+"--Ceo of Reliance Industries");
+}
+
+}
