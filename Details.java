8ee8dc69ad49fc6f17cf6e095767d951da11b976
Krishna Kumar N <113898309+Krishnakissi@users.noreply.github.com>
2023-01-24 20:38:33 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,17 @@
+class Details{
+static int age(String name,long Number)
+
+{
+if(name=="Krishna" && Number==8748917604L)
+{
+	return 24;
+}
+
+if(name=="Jashu" && Number==7204300420L)
+{
+	return 27;
+}
+
+	return 0;
+}
+}
---
+++ DetailsRunner.java
@@ -0,0 +1,11 @@
+class DetailsRunner{
+public static void main(String[]values)
+{
+	int ref=Details.age("Krishna",8748917604L);
+	System.out.println(ref);
+	
+	int ref1=Details.age("Jashu",7204300420L);
+	System.out.println(ref1);
+}
+
+}
