9778215e03435fb2badce2b90c17976f26f72b19
Krishna Kumar N <113898309+Krishnakissi@users.noreply.github.com>
2023-01-24 20:44:24 +0530

    Add files via upload

---
+++ DisplayNumber.java
@@ -0,0 +1,20 @@
+class DisplayNumber{
+
+static long mobileNumber(String name,String names)
+{
+	
+if(name=="Krishna")
+{
+	return 7676389711L;
+}
+
+if (names=="Akarsh")
+{
+	return 9353804220lL;
+}
+
+return 0;
+
+}
+
+}
---
+++ DisplayNumberRunner.java
@@ -0,0 +1,8 @@
+class DisplayNumberRunner{
+
+public static void main(String[]values)
+{
+long ref=DisplayNumber.mobileNumber("Krishna","Akarsh");
+System.out.println(ref);
+}
+}
